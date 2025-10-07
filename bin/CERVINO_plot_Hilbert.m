8ede958846c5a277878ac77b3dd0da957e7874a9
Jan Beutel <jan.beutel@uibk.ac.at>
2025-10-07 18:22:39 +0200

    added interim processing

--- bin/CERVINO_plot_Hilbert.m
+++ bin/CERVINO_plot_Hilbert.m
@@ -2,7 +2,7 @@ close all
 clear all
 clc
 
-year    = "2018";
+year    = "2019";
 network = "1I";
 station = "MH44";
 
@@ -20,21 +20,18 @@ fprintf("Running CERVINO_plot_hibert for year %s, station %s, %d\n", year, stati
 
 
 channel = "EHE.D";
-
-filename=["fft_" + station + "_" + channel + "_" + year + ".mat"];
+filename=["fft_" + station + "_" + channel + "_" + year + ".mat"]
 load(data_directory + filename) 
 FFT_E=FFT_all;
 DV_E=DVec;
 
 channel = "EHN.D";
-
 filename=["fft_" + station + "_" + channel + "_" + year + ".mat"];
 load(data_directory + filename) 
 FFT_N=FFT_all;
 DV_N=DVec;
 
 channel = "EHZ.D";
-
 filename=["fft_" + station + "_" + channel + "_" + year + ".mat"];
 load(data_directory + filename) 
 FFT_V=FFT_all;
@@ -72,8 +69,8 @@ Res=100;        %Resolution of PDF
 THETA=theta*pi/180;
 Theta=num2str(theta);
 
-        fmin=0.1; %Hz
-        fmax=100; %Hz
+fmin=0.1; %Hz
+fmax=100; %Hz
 
 %Bins definition
 %vecbinf=logspace(-3,2,100); %vecteur espace regulierement en log, entre 10^-3 et 100 Hz, avec 10 points par decade
@@ -88,96 +85,75 @@ vecbinpow=vecbinpow(:);
 %     vect_f=logspace(log10(fmin),log10(fmax),500)';
 vect_f=VECT_F;
 
-    %Initialisation matrice PDF
-    Super_PDF_mat=zeros(length(vecbinpow),length(vecbinf));
-    Stats_HV_mat=zeros(size(FFT_E_c));
-            
-                    %Selection de la composante verticale, nord et est
-                    Super_fft_V=FFT_V_c;
-                    Super_fft_N=FFT_N_c;
-                    Super_fft_E=FFT_E_c;
-                    
-                        for ii=1:length(Super_fft_V(1,:));
-                            SV=Super_fft_V(:,ii);
-                            SN=Super_fft_N(:,ii);
-                            SE=Super_fft_E(:,ii);
-                            
-                            % Spectres horizontal
-                            switch type_HV
-                                case 'SA';   SH=sqrt((SN.^2+SE.^2)/2);
-                                case 'THE';  SH=sqrt(SN.^2+SE.^2);
-                                case 'DE';
-                                    if isnan(theta)
-                                        disp('Warning theta not defined');
-                                    else
-                                        SH=(SN.*cos(THETA))+(SE.*sin(THETA));
-                                    end
-                                case 'E'
-                                    SH=SE;
-                            end
-                            
-                            if sum(SV)~=0 && sum(SH)~=0
-                                HV=SH./SV;
-                                HV=HV(:);                 
-                                if max(HV)<=512 && min(HV)>=0.0625
-%                                 Stats_HV_mat=[Stats_HV_mat HV];
-                                [Matrix_Counts] = func_hist2d_noc(vect_f,HV,vecbinf,vecbinpow);
-                                Super_PDF_mat=Super_PDF_mat+Matrix_Counts;
-                                Stats_HV_mat(:,ii)=HV;
+%Initialisation matrice PDF
+Super_PDF_mat=zeros(length(vecbinpow),length(vecbinf));
+Stats_HV_mat=zeros(size(FFT_E_c));
+        
+                %Selection de la composante verticale, nord et est
+                Super_fft_V=FFT_V_c;
+                Super_fft_N=FFT_N_c;
+                Super_fft_E=FFT_E_c;
+                
+                    for ii=1:length(Super_fft_V(1,:));
+                        SV=Super_fft_V(:,ii);
+                        SN=Super_fft_N(:,ii);
+                        SE=Super_fft_E(:,ii);
+                        
+                        % Spectres horizontal
+                        switch type_HV
+                            case 'SA';   SH=sqrt((SN.^2+SE.^2)/2);
+                            case 'THE';  SH=sqrt(SN.^2+SE.^2);
+                            case 'DE';
+                                if isnan(theta)
+                                    disp('Warning theta not defined');
+                                else
+                                    SH=(SN.*cos(THETA))+(SE.*sin(THETA));
                                 end
+                            case 'E'
+                                SH=SE;
+                        end
+                        
+                        if sum(SV)~=0 && sum(SH)~=0
+                            HV=SH./SV;
+                            HV=HV(:);                 
+                            if max(HV)<=512 && min(HV)>=0.0625
+%                                 Stats_HV_mat=[Stats_HV_mat HV];
+                            [Matrix_Counts] = func_hist2d_noc(vect_f,HV,vecbinf,vecbinpow);
+                            Super_PDF_mat=Super_PDF_mat+Matrix_Counts;
+                            Stats_HV_mat(:,ii)=HV;
                             end
                         end
+                    end
 
-    %Normalisation probas
-    N_occurences_tot=sum(Super_PDF_mat,1);
-    
-    %Normalisation
-    for ind_f=1:length(vecbinf)
-        Super_PDF_mat(:,ind_f)=Super_PDF_mat(:,ind_f)/N_occurences_tot(ind_f);
-    end
-    
-    %Stats
-    a=find(Stats_HV_mat(100,:)==0);
-    mat=Stats_HV_mat;                             
-    mat(:,a)=[];
-    DV_short=DVc;
-    DV_short(a)=[];
+%Normalisation probas
+N_occurences_tot=sum(Super_PDF_mat,1);
+
+%Normalisation
+for ind_f=1:length(vecbinf)
+    Super_PDF_mat(:,ind_f)=Super_PDF_mat(:,ind_f)/N_occurences_tot(ind_f);
+end
+
+%Stats
+a=find(Stats_HV_mat(100,:)==0);
+mat=Stats_HV_mat;                             
+mat(:,a)=[];
+DV_short=DVc;
+DV_short(a)=[];
 
 
 
-% function CERVINO_classificazione(year, station, channel)
-   % year comes in as a number (e.g. 2018)
-%    year = string(year);
+channel = "EHN.D";
 
-%    close all
-%    clearvars -except year station channel  % don’t clear year
+data_directory = "../results/classification/" + network + "/" + station + "/" + year + "/";
+% List all .miniseed files in the directory
+FileList = dir(fullfile(data_directory, '*.mat'));
+TOT = size(FileList,1);
 
-% year    = "2018";
-% network = "1I";
-% station = "MH44";
-% channel = "EHE.D";
-% 
-% data_directory = "../results/classification/" + network + "/" + station + "/" + year + "/";
-% % List all .miniseed files in the directory
-% FileList = dir(fullfile(data_directory, '*.mat'));
-% TOT = size(FileList,1);
-% 
-% savedir = "../results/plots/";
-% if ~exist(savedir, 'dir')  % check if folder exists
-%     mkdir(savedir);         % create folder if it doesn't exist
-% end
-% 
-% fprintf("Running CERVINO_plot_hibert for year %s, station %s, channel %s, %d\n", year, station, channel, TOT)
-% 
-% % get the file name:
-% % folder = FileList(ite).folder;
-% % filename = FileList(ite).name;
-% 
-% filename=["classification_" + station + "_" + channel + "_" + year + ".mat"];
-% load(data_directory + filename) 
+filename=["classification_" + station + "_" + channel + "_" + year + ".mat"]
+load(data_directory + filename) 
 
 % cd('C:\Users\Valer\OneDrive - Politecnico di Torino\CERVINO RISULTATI\2015-2025\MH36\microseismicity')
-load('CLASS_MH44_N.mat');   %%%%%%%%%%%%%%%
+% load('CLASS_MH44_N.mat');   %%%%%%%%%%%%%%%
 load('fieldwork_dates.mat');
 % load('rockfall_dates.mat');
 % load('helicopter_dates.mat');
@@ -199,9 +175,10 @@ end_fw=datenum(end_fw);
 % end_eli=datenum(end_eli);
 
 
+minDv=min(Dv);
+maxDv=max(Dv);
+
 % time=datenum(time);
-minDv=min(Dv)
-maxDv=max(Dv)
 % idx=(time >= minDv & time <= maxDv);
 % time=time(idx);
 % T_100cm=T_100cm(idx);
@@ -209,10 +186,6 @@ maxDv=max(Dv)
 % idx2=(DVc >=minDv & DVc<=maxDv);
 % DVc=DVc(idx);
 % Stats_HV_mat=Stats_HV_mat(:,idx);
-
-
-
-
 
 % DVec=DVec1;
 % a=find(Hilbert(:,9)>35);
@@ -238,7 +211,7 @@ hold on
 % ylabel('T (°C)')
 % datetick('x','mm/yy', 'keeplimits')
 
-subplot(2, 1,1)
+subplot(3,1,1)
 plot(Dv, data(:,9),'.')
 datetick('x','mm/yy', 'keeplimits')
 xlim([minDv maxDv])
@@ -250,7 +223,7 @@ for i = 1:length(start_fw)
     plot(Dv(idx), data(idx,9), 'r.', 'MarkerSize', 12)
 end
 
-subplot(2, 1,2)
+subplot(3,1,2)
 plot(Dv_f, data_f(:,9),'.')
 datetick('x','mm/yy', 'keeplimits')
 xlim([minDv maxDv])
@@ -260,22 +233,22 @@ hold on
 hold off
 
 
-% subplot(4,1,4)
-% imagesc(DVc,VECT_F,Stats_HV_mat)
-% set(gca,'Yscale','log')
-% set(gca,'Ydir','normal')
-% xlim([minDv maxDv])
-% ylim([5 50])
-% datetick('x','mm/yy','keeplimits')
-% xlabel('Date (mm/yy)')
-% ylabel('Frequency (Hz)')
-% load('FFT_cmap.mat')
-% colormap(cmap)
-% c=colorbar
-% c.Label.String='HV'
-% caxis([0 10])
-% set(gca,'layer','top')
-% box on
+subplot(3,1,3)
+imagesc(DVc,VECT_F,Stats_HV_mat)
+set(gca,'Yscale','log')
+set(gca,'Ydir','normal')
+xlim([minDv maxDv])
+ylim([5 50])
+datetick('x','mm/yy','keeplimits')
+xlabel('Date (mm/yy)')
+ylabel('Frequency (Hz)')
+load('FFT_cmap.mat')
+colormap(cmap);
+c=colorbar;
+c.Label.String='HV'
+caxis([0 10])
+set(gca,'layer','top')
+box on
 % sdf('20_15')
 
 
